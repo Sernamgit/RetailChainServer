18d7fbce11e865656fbbc1b14d0658005e8678ef
Sernam <wasl@list.ru>
2025-04-08 22:19:20 +0300

    добавлен gradle-wrapper

--- src/main/java/ru/otus/prof/retail/RetailApplication.java
+++ src/main/java/ru/otus/prof/retail/RetailApplication.java
@@ -1,24 +1,14 @@
 package ru.otus.prof.retail;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class RetailApplication implements CommandLineRunner {
-    private static final Logger log = LoggerFactory.getLogger(RetailApplication.class);
-
+public class RetailApplication {
     public static void main(String[] args) {
         SpringApplication.run(RetailApplication.class, args);
-
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
 
     }
 }
